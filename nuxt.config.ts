cf3c0473ef594f5babaaed9f6874c4c6a38bcad4
irinakurnishova <kurnishovaiv@gmail.com>
2026-07-19 18:14:32 +0200

    init: nuxt 3 + tailwind + pwa + supabase setup

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt',
+  ],
+  supabase: {
+    redirect: false,
+  },
+  pwa: {
+    manifest: {
+      name: 'Velvet',
+      short_name: 'Velvet',
+      theme_color: '#0D0D0D',
+      background_color: '#0D0D0D',
+      icons: [
+        { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+        { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+  },
+  devtools: { enabled: true },
+})
